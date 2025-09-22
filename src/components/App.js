109956252fd2987c9fa1e7cb358fde2be875dd81
TusharMinche <tusharminche@gmail.com>
2025-09-22 12:17:20 +0530

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./../redux/actions";
 
 const App = () => {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>{count}</h1>
+
+      <div>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button onClick={() => dispatch(decrement())}>decrement</button>
+      </div>
     </div>
   )
 }
